dd85edd0f63b3eaa7d23cd53a5f493b9e992da12
Blair Granville <blair.granville+git@gmail.com>
2021-04-15 15:30:48 +0100

    Fixed name and packages= in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='useful_sql',
+   name='sql_query_functions',
    version='1.0',
    description='My personal module for doing some common tasks with PYODBC',
    author='Blair Granville',
    author_email='blair.granville+github@gmail.com',
-   packages=['useful_sql'],  #same as name
+   packages=['sql_query_functions'],  #same as name
    install_requires=['pyodbc', 'pandas'], #external packages as dependencies
 )
